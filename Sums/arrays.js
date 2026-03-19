9acb15772687a32f2e4c93549a1b25b0361d4a45
Akash <aakash.velu.003@gmail.com>
2026-03-20 00:58:13 +0530

    Q2: Sort the Array without using builtin functions

--- Sums/arrays.js
+++ Sums/arrays.js
@@ -1,15 +1,32 @@
 //? Q1: Print the 2d Arrays
-let arr = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-];
+// let arr = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
 
-for (let row = 0; row < arr.length; row++) {
-  for (let col = 0; col < arr[row].length; col++) {
-    console.log(arr[row][col]);
-  }
-}
+// for (let row = 0; row < arr.length; row++) {
+//   for (let col = 0; col < arr[row].length; col++) {
+//     console.log(arr[row][col]);
+//   }
+// }
 
 //? Q2: Sort the Array without using builtin functions
+
 // let arr2 = [42, 7, 95, 13, 68, 31, 84, 56, 19, 77];
+// let arr1 = [...arr2]; // & optional just to save original value if need using spread only works
+
+// for (let i = 0; i < arr1.length - 1; i++) {
+//   for (j = 0; j < arr1.length - 1 - i; j++) {
+//     if (arr1[j] > arr1[j + 1]) {
+//       //* if the Left num is Greater than the right num SWAP! happens
+
+//       let temp = arr1[j]; //* Step 1: Save left value in a temp box
+//       arr1[j] = arr1[j + 1]; //* Step 2: Put right value into left position
+//       arr1[j + 1] = temp; //*Step 3: Put saved value into right position
+//     }
+//   }
+// }
+
+// console.log(arr2);
+// console.log(arr1);
