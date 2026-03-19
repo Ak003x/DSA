adf6d30dd6999cbebaae100f915d5050d8695e0a
Akash <aakash.velu.003@gmail.com>
2026-03-19 20:57:38 +0530

    2arrays

---
+++ Sums/arrays.js
@@ -0,0 +1,15 @@
+//? Q1: Print the 2d Arrays
+let arr = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+for (let row = 0; row < arr.length; row++) {
+  for (let col = 0; col < arr[row].length; col++) {
+    console.log(arr[row][col]);
+  }
+}
+
+//? Q2: Sort the Array without using builtin functions
+// let arr2 = [42, 7, 95, 13, 68, 31, 84, 56, 19, 77];
