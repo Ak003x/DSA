1dcf00ac06d0fb4fb668e5d507c95fee5384ad21
Ak <aakash.velu.003@gmail.com>
2026-05-30 00:58:59 +0530

    sums

---
+++ Sums/sums/occurrences_ofEach_str.js
@@ -0,0 +1,14 @@
+function Occurrences(str) {
+  const counts = {};
+  //   for (let i = 0; i < str.length; i++) { Using for loop
+  for (let char of str) {              //Using for of loop
+     if (counts[char]) {
+      counts[char]++;
+    } else {
+      counts[char] = 1;
+    }
+  }
+  return counts;
+}
+
+console.log(Occurrences("akash"));
---
+++ Sums/sums/palidrome.js
@@ -0,0 +1,9 @@
+function Palindrome(str) {
+  let reverse = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverse += str[i];
+  }
+  console.log(reverse.toLowerCase() === str.toLowerCase() ? true : false);
+}
+
+Palindrome("MOM");
